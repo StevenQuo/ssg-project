da5c7381eab742f3fa37e6e55f6f8c4c322b6a88
StevenQuo <quosteven@gmail.com>
2025-10-01 15:32:48 +0800

    Add Product Detail page template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
-import PostsSlider from "@/ui/components/PostsSlider";
+import PostsPage from "./posts/page";
+import ProductsPage from "./products/page";
 
 export default function Home() {
   return (
     <section>
-      <PostsSlider />
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        E-Commerce
-      </h1>
-      <p className="mb-4">
-        {`Lorem Ipsum Dolor sit Amet`}
-      </p>
-      <div className="my-8">
-      </div>
+      <ProductsPage />
+      <PostsPage />
     </section>
   );
 }
--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,5 +1,7 @@
 import { getProduct } from "@/lib/products";
 import { generateProductParams } from "@/lib/params/products-params";
+import Image from "next/image";
+import Link from "next/link";
 
 type Props = {
     params: Promise<{ id: string }>;
@@ -14,23 +16,53 @@ export default async function ProductDetailPage({ params }: Props) {
   const { id } = await params;
   const product = await getProduct(id);
 
+  const message = `Halo, saya ingin memesan *${product.title}* dengan rincian:
+  Harga: $ ${product.price};
+  Kategori: ${product.category};
+  Brand: ${product.brand};
+  Berat: ${product.weight};
+  Dimensi: 
+    Lebar: ${product.dimensions.width} inch
+    Tinggi: ${product.dimensions.height} inch
+
+  Terima kasih.`;
+
+  const encodedMessage = encodeURIComponent(message);
+
   return (
-    <main className="flex items-center w-full h-screen">
-      <div className="flex flex-col gap-8 m-8">
-        <h1 className="title">{product.title}</h1>
-        <div className="">
-            <p className="text-3xl text-green-600 font-semibold">${product.price}</p>
-            <p className="text-sm text-gray-500">
-                Category: {product.category}
-            </p>
-            <p>{product.description}</p>
-        </div>
-        <div className="">
-            <a className="bg-blue-500 hover:bg-blue-700 transition-all duration-300 ease-in-out text-white font-bold py-2 px-4 rounded" href="/products/">
-                Kembali
-            </a>
-        </div>
-      </div>
-    </main>
+    <section className="mx-auto grid max-w-7xl p-8">
+			<form className="grid gap-2 sm:grid-cols-2 lg:grid-cols-8">
+				<div className="md:col-span-1 lg:col-span-5">
+					{product.images[0] && (
+						<Image
+							priority={true}
+							alt={product.title ?? ""}
+							width={1024}
+							height={1024}
+							src={product.images[0] ?? ""}
+						/>
+					)}
+				</div>
+				<div className="flex flex-col pt-6 sm:col-span-1 sm:px-6 sm:pt-0 lg:col-span-3 lg:pt-16">
+					<div>
+						<h1 className="mb-4 flex-auto text-3xl font-medium tracking-tight text-neutral-900">
+							{product.title}
+						</h1>
+						<p className="mb-8 text-sm " data-testid="ProductElement_Price">
+							$ {product.price}
+						</p>
+
+						<div className="mt-8">
+              <Link className="px-4 py-2 border border-black text-black rounded-lg transition-colors duration-200 dark:border-white dark:text-white hover:bg-[#25D366] hover:text-white hover:border-[#25D366] whitespace-nowrap" href={`https://wa.me/6281329578424?text=${encodedMessage}`}>Order by WhatsApp</Link>
+						</div>
+						{product.description && (
+							<div className="mt-8 space-y-6 text-sm text-neutral-500">
+								{product.description}
+							</div>
+						)}
+					</div>
+				</div>
+			</form>
+		</section>
   );
 }
